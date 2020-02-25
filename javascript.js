e9f0661461b09a5e54c1e4cec2794ad853937957
Trusca Alexandru <54893519+alextrusca@users.noreply.github.com>
2020-02-25 11:22:10 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    alert("Buna ziua! Bine ati venit pe pagina noastra!");
+}
+function momIndisp(){
+    alert(`Rezultatele  sunt indisponibile momentan!
+    Va rugam, reveniti!`);
+}
